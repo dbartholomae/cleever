820c68b17d70e148e7f720a3d3d7bff946364b6b
Daniel Bartholomae <daniel@bartholomae.name>
2022-07-30 17:36:41 +0200

    Collect cards in a map
    Co-authored-by: Daniel Bartholomae <daniel@bartholomae.name>
    Co-authored-by: Elisabeth <elisabeth.aliena@gmail.com>

--- src/Tableau.js
+++ src/Tableau.js
@@ -5,39 +5,18 @@ export const BOTTOM_RIGHT = Symbol("bottom-right");
 
 export class Tableau {
   openPlacementCount = 4;
-  topLeftCard;
-  topRightCard;
-  bottomLeftCard;
-  bottomRightCard;
+  #cards;
+
+  constructor() {
+    this.#cards = new Map();
+  }
 
   placeCard(card, position) {
     this.openPlacementCount--;
-    switch (position) {
-      case TOP_LEFT:
-        this.topLeftCard = card;
-        return;
-      case TOP_RIGHT:
-        this.topRightCard = card;
-        return;
-      case BOTTOM_LEFT:
-        this.bottomLeftCard = card;
-        return;
-      case BOTTOM_RIGHT:
-        this.bottomRightCard = card;
-        return;
-    }
+    this.#cards.set(position, card);
   }
 
   getCard(position) {
-    switch (position) {
-      case TOP_LEFT:
-        return this.topLeftCard;
-      case TOP_RIGHT:
-        return this.topRightCard;
-      case BOTTOM_LEFT:
-        return this.bottomLeftCard;
-      case BOTTOM_RIGHT:
-        return this.bottomRightCard;
-    }
+    return this.#cards.get(position);
   }
 }
