fca632a6f9d203a80e365205469ea9be7872f4d4
Daniel Bartholomae <daniel@bartholomae.name>
2022-07-30 17:21:19 +0200

    Allow to place top-left card
    Co-authored-by: Daniel Bartholomae <daniel@bartholomae.name>
    Co-authored-by: Elisabeth <elisabeth.aliena@gmail.com>

--- src/Tableau.js
+++ src/Tableau.js
@@ -1,3 +1,11 @@
+export const TOP_LEFT = Symbol('top-left');
+
 export class Tableau {
   openPlacementCount = 4;
+  topLeftCard;
+
+  placeCard(card) {
+    this.openPlacementCount--;
+    this.topLeftCard = card;
+  }
 }
--- src/Tableau.test.js
+++ src/Tableau.test.js
@@ -1,8 +1,23 @@
-import {Tableau} from './Tableau.js';
+import {Tableau, TOP_LEFT} from './Tableau.js';
+import {Card} from './Card.js';
 
 describe('Tableau', () => {
   it('has 4 open card placements', () => {
     const tableau = new Tableau();
     expect(tableau.openPlacementCount).toBe(4);
   });
+
+  it('has 3 open card placements after placing a card', () => {
+    const tableau = new Tableau();
+    tableau.placeCard(new Card());
+    expect(tableau.openPlacementCount).toBe(3);
+  });
+
+  it('has the card in the top left slot that was placed in the top left slot',
+      () => {
+        const card = new Card();
+        const tableau = new Tableau();
+        tableau.placeCard(card, TOP_LEFT);
+        expect(tableau.topLeftCard).toEqual(card);
+      });
 });
