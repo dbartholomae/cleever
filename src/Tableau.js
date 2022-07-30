cd2941ae5783c3ecbc652be249885bae3cb30a8f
Daniel Bartholomae <daniel@bartholomae.name>
2022-07-30 17:48:48 +0200

    Allow adding descriptions to all sides
    Co-authored-by: Daniel Bartholomae <daniel@bartholomae.name>
    Co-authored-by: Elisabeth <elisabeth.aliena@gmail.com>

--- src/Tableau.js
+++ src/Tableau.js
@@ -3,12 +3,19 @@ export const TOP_RIGHT = Symbol("top-right");
 export const BOTTOM_LEFT = Symbol("bottom-left");
 export const BOTTOM_RIGHT = Symbol("bottom-right");
 
+export const LEFT = Symbol("left");
+export const RIGHT = Symbol("right");
+export const TOP = Symbol("top");
+export const BOTTOM = Symbol("bottom");
+
 export class Tableau {
   openPlacementCount = 4;
   #cards;
+  #descriptions;
 
   constructor() {
     this.#cards = new Map();
+    this.#descriptions = new Map();
   }
 
   placeCard(card, position) {
@@ -18,5 +25,13 @@ export class Tableau {
 
   getCard(position) {
     return this.#cards.get(position);
+  }
+
+  addDescription(description, position) {
+    this.#descriptions.set(position, description);
+  }
+
+  getDescription(position) {
+    return this.#descriptions.get(position);
   }
 }
--- src/Tableau.test.js
+++ src/Tableau.test.js
@@ -1,7 +1,11 @@
 import {
+  BOTTOM,
   BOTTOM_LEFT,
   BOTTOM_RIGHT,
+  LEFT,
+  RIGHT,
   Tableau,
+  TOP,
   TOP_LEFT,
   TOP_RIGHT,
 } from "./Tableau.js";
@@ -26,6 +30,16 @@ describe("Tableau", () => {
       const tableau = new Tableau();
       tableau.placeCard(card, position);
       expect(tableau.getCard(position)).toEqual(card);
+    }
+  );
+
+  it.each([TOP, BOTTOM, LEFT, RIGHT])(
+    "has the added description in the %s after adding it there",
+    (position) => {
+      const tableau = new Tableau();
+      const description = "Schnuffelpu";
+      tableau.addDescription(description, position);
+      expect(tableau.getDescription(position)).toBe(description);
     }
   );
 });
