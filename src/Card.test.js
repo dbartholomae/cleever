9d2df9f790aac8f43ec6dd776629055adf9ed361
Daniel Bartholomae <daniel@bartholomae.name>
2022-07-30 16:53:18 +0200

    Extract words out of tests
    Co-authored-by: Daniel Bartholomae <daniel@bartholomae.name>
    Co-authored-by: Elisabeth <elisabeth.aliena@gmail.com>

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,33 +1,24 @@
 import {Card} from './Card.js';
 
 describe('Card', () => {
+  const words = [
+    'bulemie',
+    'alphabet',
+    'chlamydien',
+    'dyskinesie'];
+
   it('starts with its first word up', () => {
-    const words = [
-      'bulemie',
-      'alphabet',
-      'chlamydien',
-      'dyskinesie'];
     const card = new Card(words);
     expect(card.up).toBe(words[0]);
   });
 
   it('has the second word up after being rotated counterclockwise once', () => {
-    const words = [
-      'bulemie',
-      'alphabet',
-      'chlamydien',
-      'dyskinesie'];
     const card = new Card(words);
     card.rotateCounterclockwise();
     expect(card.up).toBe(words[1]);
   });
 
   it('has the first word up after being rotated counterclockwise and clockwise once each', () => {
-    const words = [
-      'bulemie',
-      'alphabet',
-      'chlamydien',
-      'dyskinesie'];
     const card = new Card(words);
     card.rotateCounterclockwise();
     card.rotateClockwise();
