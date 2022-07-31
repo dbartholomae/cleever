381332ffa56f364a22fbb20670734e65d4ae22de
elisabethaliena <elisabeth.aliena@gmail.com>
2022-07-31 16:51:50 +0200

    update Card to be able to discern rotated cards

--- src/Card.test.js
+++ src/Card.test.js
@@ -49,5 +49,12 @@ describe("Card", () => {
       const card = new Card(words);
       expect(card.equals(card)).toBe(true);
     })
+
+    it("doesn't accept two cards with the same words as equal if one of them is rotated", () => {
+      const card = new Card(words);
+      const cardRotated = new Card(card.up, card.right, card.down, card.left);
+      cardRotated.rotateClockwise();
+      expect(card.equals(cardRotated)).toBe(false);
+    })
   });
 });
