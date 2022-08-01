c92e94734ed0f1d2129df4e3266be6ac3336d45b
Daniel Bartholomae <daniel@bartholomae.name>
2022-08-01 23:29:40 +0200

    Make Card look nice
    Co-authored-by: Daniel Bartholomae <daniel@bartholomae.name>
    Co-authored-by: elisabethaliena <elisabeth.aliena@gmail.com>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import * as React from "react";
+import styles from "./Card.module.css";
 
 export const Card = ({ card }) => (
-  <div>
-    <span>{card.up}</span>
-    <span>{card.right}</span>
-    <span>{card.down}</span>
-    <span>{card.left}</span>
+  <div className={styles.card}>
+    <span className={`${styles.up} ${styles.word}`}>{card.up}</span>
+    <span className={`${styles.right} ${styles.word}`}>{card.right}</span>
+    <span className={`${styles.down} ${styles.word}`}>{card.down}</span>
+    <span className={`${styles.left} ${styles.word}`}>{card.left}</span>
   </div>
 );
--- src/components/Card.stories.jsx
+++ src/components/Card.stories.jsx
@@ -4,5 +4,7 @@ import { Card as GameCard } from "../game/Card.js";
 import { Card } from "./Card.jsx";
 
 export const Default = () => (
-  <Card card={new GameCard(["bebe", "otter bebe", "lisa", "benni"])} />
+  <Card
+    card={new GameCard(["Schokolade", "Drachentattoo", "Anhalter", "Goldesel"])}
+  />
 );
