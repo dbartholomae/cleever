277b0ecc49612c4e064d8ff5044a6255d3ffe385
Daniel Bartholomae <daniel@bartholomae.name>
2022-08-01 23:32:00 +0200

    Mock out CSS files in tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ export default {
   transform: {
     "^.+\\.(t|j)sx?$": ["@swc/jest"],
   },
+  moduleNameMapper: {
+    "^.+\\.css$": "<rootDir>/src/test/fileStub.js",
+  },
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
